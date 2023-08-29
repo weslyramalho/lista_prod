ebaa29fe140f7fe7e0cbac7dbedcefee62499ac3
weslyramalho <weslyramalho@yahoo.com.br>
2023-08-29 18:04:13 -0300

    first commit

---
+++ app/src/main/java/br/com/wr/listadecompras/ui/activity/ListaProdutosActivity.java
@@ -0,0 +1,16 @@
+package br.com.wr.listadecompras.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.wr.listadecompras.R;
+
+public class ListaProdutosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_produtos);
+    }
+}
